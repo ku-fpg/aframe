168f9fcc33f55c7708d21117a034f32b233c5eba
Andy Gill <andygill@ku.edu>
2016-04-24 12:09:09 -0500

    more complete example

--- src/Text/AFrame/DSL.hs
+++ src/Text/AFrame/DSL.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE GADTs #-}
--- | Small monadic/applicative DSL for AFrame
+-- | Small monadic DSL for AFrame generation.
 module Text.AFrame.DSL where
  
 import Control.Monad
@@ -8,17 +8,18 @@ import Text.AFrame
 
 scene :: DSL () -> AFrame
 scene m = case run prog 0 of
-             ([],[f]) -> f
+             ([], [f], _) -> f
+             (_,  [],  _) -> error "scene internal error: no top-level primitive"
+             (_,  [_], _) -> error "scene internal error: top-level attribute"
+             (_,  _,   _) -> error "scene internal error: to many top-level primitives"
   where prog = prim "scene" m
-        run :: DSL () -> Int -> ([(Attribute,Property)],[AFrame])
-        run (Pure a) _ = ([],[])
-        run (Bind (Node nm m0) m) i = (attrs,AFrame (Primitive nm) attrs0 frames0:frames)
-          where  (attrs,frames) = run m i
-                 (attrs0,frames0) = run m0 i
-        run (Bind (Attr a p) m) i = ((a,p):attrs,frames)
-          where (attrs,frames) = run m i
-        
---  AFrame (Primitive "scene") [] []
+        run :: DSL () -> Int -> ([(Attribute,Property)],[AFrame],Int)
+        run (Pure a) i = ([],[],i)
+        run (Bind (Node nm m0) m) i0 = (attrs,AFrame (Primitive nm) attrs0 frames0:frames,i2)
+          where (attrs0,frames0,i1) = run m0 i0
+                (attrs,frames,i2)   = run m i1
+        run (Bind (Attr a p) m) i0 = ((a,p):attrs,frames,i1)
+          where (attrs,frames,i1) = run m i0
 
 data DSL a where
   Pure :: a               -> DSL a
@@ -49,10 +50,7 @@ attr :: Attribute -> Property -> DSL ()
 attr a p = Bind (Attr a p) (Pure ())
 
 example :: AFrame 
-example = scene $ 
+example = scene $ do
   attr "Hello" "World"
-
-
-
-
-
+  prim "foo" $ do
+    attr "This" "That"
