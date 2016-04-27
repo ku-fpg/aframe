ff97028d93d37790d6f3c8c6dae4e12fb5fe3f8d
Andy Gill <andygill@ku.edu>
2016-04-27 09:31:35 -0500

    Make replace Label and Property with single node Attribute in diff engine

--- src/Text/AFrame.hs
+++ src/Text/AFrame.hs
@@ -55,13 +55,12 @@ data AFrameFamily :: * -> * -> * where
  AFrame'     ::              AFrameFamily AFrame      (Cons Primitive 
                                                       (Cons [Attribute] 
                                                       (Cons [AFrame] Nil)))
- ConsAttr'   ::              AFrameFamily [Attribute] (Cons Label (Cons Property (Cons [Attribute] Nil)))
+ ConsAttr'   ::              AFrameFamily [Attribute] (Cons Attribute (Cons [Attribute] Nil))
  NilAttr'    ::              AFrameFamily [Attribute] Nil
  ConsAFrame' ::              AFrameFamily [AFrame] (Cons AFrame (Cons [AFrame] Nil))
  NilAFrame'  ::              AFrameFamily [AFrame] Nil
  Primitive'  :: Primitive -> AFrameFamily Primitive Nil
- Label'      :: Label     -> AFrameFamily Label Nil
- Property'   :: Property  -> AFrameFamily Property Nil
+ Attribute'  :: Attribute -> AFrameFamily Attribute Nil
 
 instance Family AFrameFamily where
   decEq  :: AFrameFamily tx txs -> AFrameFamily ty tys -> Maybe (tx :~: ty, txs :~: tys)
@@ -72,35 +71,31 @@ instance Family AFrameFamily where
   decEq NilAFrame'  NilAFrame'  = Just (Refl, Refl)
 
   decEq (Primitive' p1) (Primitive' p2) | p1 == p2 = Just (Refl, Refl)
-  decEq (Label' l1)     (Label' l2)     | l1 == l2 = Just (Refl, Refl)
-  decEq (Property' p1)  (Property' p2)  | p1 == p2 = Just (Refl, Refl)
-
+  decEq (Attribute' a1) (Attribute' a2) | a1 == a2 = Just (Refl, Refl)
   decEq _           _           = Nothing
 
   fields :: AFrameFamily t ts -> t -> Maybe ts
   fields AFrame'        (AFrame prim attrs fs) 
                            = Just $ CCons prim $ CCons attrs $ CCons fs $ CNil
   fields ConsAttr'      ((lbl,prop):xs) 
-                           = Just $ CCons lbl $ CCons prop $ CCons xs $ CNil
+                           = Just $ CCons (lbl,prop) $ CCons xs $ CNil
   fields NilAttr'       [] = Just CNil
   fields ConsAFrame'    (x:xs) 
                            = Just $ CCons x $ CCons xs $ CNil
   fields NilAFrame'     [] = Just CNil
   fields (Primitive' _) _  = Just CNil
-  fields (Label'     _) _  = Just CNil
-  fields (Property'  _) _  = Just CNil
+  fields (Attribute' _) _  = Just CNil
   fields _              _  = Nothing
 
   apply  :: AFrameFamily t ts -> ts -> t
   apply AFrame'         (CCons prim (CCons attrs (CCons fs CNil)))
                              = AFrame prim attrs fs
-  apply ConsAttr'       (CCons lbl (CCons prop (CCons xs CNil))) = (lbl,prop) : xs
+  apply ConsAttr'       (CCons (lbl,prop) (CCons xs CNil)) = (lbl,prop) : xs
   apply NilAttr'        CNil = []
   apply ConsAFrame'     (CCons x (CCons xs CNil)) = x : xs
   apply NilAFrame'      CNil = []
   apply (Primitive' p1) CNil = p1
-  apply (Label'     l1) CNil = l1
-  apply (Property'  p1) CNil = p1
+  apply (Attribute' a1) CNil = a1
 
   string :: AFrameFamily t ts -> String
   string AFrame'         = "AFrame"
@@ -108,9 +103,8 @@ instance Family AFrameFamily where
   string NilAttr'        = "NilAttr"
   string ConsAFrame'     = "ConsAFrame"
   string NilAFrame'      = "NilAFrame"
-  string (Label' l1)     = "Label-"     ++ show l1
-  string (Primitive' p1) = "Primitive-" ++ show p1
-  string (Property' p1)  = "Property-"  ++ show p1
+  string (Primitive' l1) = show l1
+  string (Attribute' p1) = show p1
 
 
 instance Type AFrameFamily AFrame where
@@ -125,10 +119,8 @@ instance Type AFrameFamily [Attribute] where
 instance Type AFrameFamily [AFrame] where
     constructors = [Concr ConsAFrame',Concr NilAFrame']
  
-instance Type AFrameFamily Label where
-    constructors = [Abstr Label']
+instance Type AFrameFamily Attribute where
+    constructors = [Abstr Attribute']
 
-instance Type AFrameFamily Property where
-    constructors = [Abstr Property']
 
   
